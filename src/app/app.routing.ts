c09df94294733ed66c767cc60f40dc74d5bcca99
Lukas Marx <marxlukas@googlemail.com>
2018-04-10 13:37:54 +0200

    redirect to login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,8 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'projects',
